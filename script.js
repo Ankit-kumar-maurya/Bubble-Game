58d17f964facab3b9bb284ced18e6a13ae0a736e
Ankit-kumar-maurya <141322362+Ankit-kumar-maurya@users.noreply.github.com>
2023-08-16 10:01:26 +0530

    Update script.js
    music added

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 var timer = 60;
 var score = 0;
 var hitrn= 0 ;
+var totalhits = 0;
+const backmusic = new Audio('GameBackgroundSound.mp3');
+backmusic.play();
+var red = 117;
+var green = 141;
+var blue = 153;
+
+function bubbleSound(){
+    const sound = new Audio('bubbleGame.mp3');
+    sound.play();
+}
 
 function makebubble(){
     var clutter = "";
     for(var i = 1 ; i<=250 ; i++){
         var rn = Math.floor(Math.random()*10);
         clutter += `<div class="bubble">${rn}</div>`;
+        // document.getElementsByClassName('bubble').style.backgroundColor = 'rgb(red, green , blue)';
     }
     document.querySelector(".pbtm").innerHTML = clutter;
 }
@@ -20,7 +32,10 @@ function runtimer(){
         }
         else{
             clearInterval(runtimer);
-            document.querySelector(".pbtm").innerHTML = `--Game over-- <br>  Your Score : ${score}`;
+            backmusic.pause();
+            const gameover = new Audio('gameOver.mp3');
+            gameover.play();
+            document.querySelector(".pbtm").innerHTML = `--Game over-- <br> Total Bubble hitted :${totalhits} <br> Your Score : ${score}`;
         }
         
 
@@ -46,6 +61,8 @@ document.querySelector(".pbtm").addEventListener("click", function(details){
         increaseScore();
         makebubble();
         getNewHit();
+        bubbleSound();
+        totalhits++;
         
     }
     // if(hitbubble != hitrn){
